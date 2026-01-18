eff1da5737a044f2b83034d957ccdeb6f78dd01f
Nick Kubarko <kbrn.work@gmail.com>
2026-01-18 16:42:30 +0300

    initial

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Coffee Bridge</h1>
+      <div>
+        <button>Send</button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ packages/contracts/src/CoffeeERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract CoffeeERC20 is ERC20 {
+    address public owner;
+
+    function initiailize(string memory name, string memory symbol) public {
+        __initialize(name, symbol);
+    }
+}
